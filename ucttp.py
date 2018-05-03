c5225f5861a2b362cc84d238a3b8adc586036d5e
atena mohammadi <ashenatena@gmail.com>
2018-05-03 10:37:32 +0430

    add subjects file and load it

--- ucttp.py
+++ ucttp.py
@@ -3,13 +3,15 @@ import pandas as pd
 skill_file = "./information/Prof_Skill.xlsx"
 free_time_file = "./information/Proffosor_FreeTime.xlsx"
 am = "./information/Amoozesh.xlsx"
+sub = "./information/subjects.xlsx"
 
 
 def load_files():
     prof_skill = pd.read_excel(skill_file, sheet_name=None)
     prof_free_time = pd.read_excel(free_time_file, sheet_name=None)
     amoozesh = pd.read_excel(am, sheet_name=None)
-    return prof_skill, prof_free_time, amoozesh
+    subjects = pd.read_excel(sub, sheet_name=None)
+    return prof_skill, prof_free_time, amoozesh, subjects
 
 
-Proffosor_Skill, Proffosor_FreeTime, Amoozesh = load_files()
+proffosorSkill, professorFreeTime, classes, subjects = load_files()
