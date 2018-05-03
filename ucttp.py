e30a5e7fadfa3a7e3f55939deed39d124ac28103
atena mohammadi <ashenatena@gmail.com>
2018-05-03 09:48:20 +0430

    add project's information folder, load files

---
+++ ucttp.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+skill_file = "./information/Prof_Skill.xlsx"
+free_time_file = "./information/Proffosor_FreeTime.xlsx"
+am = "./information/Amoozesh.xlsx"
+
+
+def load_files():
+    prof_skill = pd.read_excel(skill_file, sheet_name=None)
+    prof_free_time = pd.read_excel(free_time_file, sheet_name=None)
+    amoozesh = pd.read_excel(am, sheet_name=None)
+    return prof_skill, prof_free_time, amoozesh
+
+
+Proffosor_Skill, Proffosor_FreeTime, Amoozesh = load_files()
